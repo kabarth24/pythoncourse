22433fac3d5b0e0a87e490e3ead2825f157089e6
Luka <lukadbarth@hotmail.com>
2023-12-15 05:35:13 -0300

    I learned about variables and how to interact with the user through 'input'

---
+++ World-1/variables.py
@@ -0,0 +1,12 @@
+nome = 'Luka'
+idade = 23
+peso = 54
+
+print(nome , idade , peso)
+
+# interagindo com o usuário
+nome = input('Qual é seu nome?')
+idade = input('Quantos anos você tem?')
+peso = input('Quanto você pesa?')
+
+print(nome , idade , peso)
