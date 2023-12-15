916d494e34373867dc30b374dc34bb0d66c958c5
Luka <lukadbarth@hotmail.com>
2023-12-15 05:58:26 -0300

    This exercise consists of asking the user for 2 numbers, making the sum between them and displaying it

---
+++ World-1/Exercises/ex004.py
@@ -0,0 +1,9 @@
+numero1 = input('Primeiro número: ')
+numero2 = input('Segundo número: ')
+
+numero1 = int(numero1)
+numero2 = int(numero2)
+
+resultado = numero1 + numero2
+
+print('A soma é', resultado)
