f383e6e5a7f81145c905702aca10794b1510b5f5
Luka <lukadbarth@hotmail.com>
2024-01-03 06:07:51 -0300

    This exercise consists of calculating the double, triple and square root of a number given by the user

---
+++ World-1/Exercises/ex007.py
@@ -0,0 +1,7 @@
+user_number = int(input('Digite um número para descobrir o dobro, o triplo e a raiz quadrada dele: '))
+
+dobro_number = user_number * 2
+triplo_number = user_number * 3
+raiz_quadrada = user_number ** 0.5
+
+print(f'O número digitado foi {user_number}. O dobro dele é {dobro_number}, o triplo é {triplo_number} e a raiz quadrada é {raiz_quadrada:.2f}')
