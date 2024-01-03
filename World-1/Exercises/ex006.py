ad8388ffd0cbf6da10e54f863a270494a2dabe84
Luka <lukadbarth@hotmail.com>
2024-01-03 05:57:17 -0300

    This exercise consists of showing the successor and predecessor of a number chosen by the user

---
+++ World-1/Exercises/ex006.py
@@ -0,0 +1,6 @@
+user_number = int(input('Digite um número inteiro para descobrir o seu sucessor e seu atencessor: '))
+
+sucessor = user_number + 1
+antecessor = user_number - 1
+
+print(f'O número digitado foi {user_number}. O seu sucessor é {sucessor} e o antecessor é {antecessor}')
