9004c5d407b2c1f20ab1db1e9f836a5de30e9529
Luka <lukadbarth@hotmail.com>
2024-01-03 05:49:44 -0300

    I learned about arithmetic operators, their order of precedence and some features of print

---
+++ World-1/arithmetic-operators.py
@@ -0,0 +1,13 @@
+adicao = 5 + 5
+subtracao = 5 - 5
+multiplicacao = 5 * 5
+divisao = 7 / 5
+divisao_inteiro = 7 // 5
+potencia = 5 ** 5
+
+print(f'Adição: {adicao}')
+print(f'Subtração: {subtracao}')
+print(f'Multiplicação: {multiplicacao}')
+print(f'Divisão: {divisao}')
+print(f'Divisão inteiro: {divisao_inteiro}')
+print(f'Potência: {potencia}')
