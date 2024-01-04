621b67df4bdfcb7daa090033b440f08478b4994a
Luka <lukadbarth@hotmail.com>
2024-01-04 05:27:08 -0300

    This exercise consists of converting a value given in meters by the user, into centimeters and millimeters

---
+++ World-1/Exercises/ex009.py
@@ -0,0 +1,8 @@
+metros = int(input('Digite um valor em metros: '))
+
+centimetro = metros * 100
+milimetro = metros * 1000
+
+print(f'Metros: {metros}m')
+print(f'Centímetros: {centimetro}cm')
+print(f'Milímetros: {milimetro}mm')
