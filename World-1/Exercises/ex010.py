0353dab8cc950809b2775e7d69e278cdd949718b
Luka <lukadbarth@hotmail.com>
2024-01-04 05:41:20 -0300

    This exercise consists of displaying on the screen the multiplication tables of a number given by the user

---
+++ World-1/Exercises/ex010.py
@@ -0,0 +1,23 @@
+numero_tabuada = int(input(f'Digite um número para obter a tabuada: '))
+
+vezes_um = numero_tabuada * 1
+vezes_dois = numero_tabuada * 2
+vezes_tres = numero_tabuada * 3
+vezes_quatro = numero_tabuada * 4
+vezes_cinco = numero_tabuada * 5
+vezes_seis = numero_tabuada * 6
+vezes_sete = numero_tabuada * 7
+vezes_oito = numero_tabuada * 8
+vezes_nove = numero_tabuada * 9
+vezes_dez = numero_tabuada * 10
+
+print(f'{numero_tabuada} x 1 = {vezes_um}')
+print(f'{numero_tabuada} x 2 = {vezes_dois}')
+print(f'{numero_tabuada} x 3 = {vezes_tres}')
+print(f'{numero_tabuada} x 4 = {vezes_quatro}')
+print(f'{numero_tabuada} x 5 = {vezes_cinco}')
+print(f'{numero_tabuada} x 6 = {vezes_seis}')
+print(f'{numero_tabuada} x 7 = {vezes_sete}')
+print(f'{numero_tabuada} x 8 = {vezes_oito}')
+print(f'{numero_tabuada} x 9 = {vezes_nove}')
+print(f'{numero_tabuada} x 10 = {vezes_dez}')
