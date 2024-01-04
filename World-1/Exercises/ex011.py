c5e515aca7df75d9dff0e3824850bab7826059c1
Luka <lukadbarth@hotmail.com>
2024-01-04 05:52:53 -0300

    This exercise consists of converting the real to dollars

---
+++ World-1/Exercises/ex011.py
@@ -0,0 +1,6 @@
+reais = float(input('Quantos reais você tem na carteira: '))
+
+conversao = reais / 3.27
+
+print(f'Cotação do dia: US$1.00 = R$3.27')
+print(f'Com {reais} reais, você pode comprar {conversao:.2f} dólares!')
