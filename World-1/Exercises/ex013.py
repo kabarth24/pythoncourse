31814d7d1e9996f23bbf8bdb240c6f9000cc6bb1
Luka <lukadbarth@hotmail.com>
2024-01-08 05:32:16 -0300

    This exercise consists of showing only the integer part of a real number given by the user

---
+++ World-1/Exercises/ex013.py
@@ -0,0 +1,7 @@
+from math import floor
+
+numero_usuario = float(input('Digite um número real: '))
+
+arredondar_numero_usuario = floor(numero_usuario)
+
+print(f'O número {numero_usuario} tem a parte inteira {arredondar_numero_usuario}.')
