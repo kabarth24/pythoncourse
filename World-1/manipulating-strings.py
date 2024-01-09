83a85fdf017531dcf8661b98d46d7c04eeb44635
Luka <lukadbarth@hotmail.com>
2024-01-09 06:11:06 -0300

    I learned about Strings, how to manipulate them, some String methods like len(), find(), replace(), lower(), upper() and etc.

---
+++ World-1/manipulating-strings.py
@@ -0,0 +1,12 @@
+frase = 'Curso em Vídeo Python'
+print(frase[:14])
+print(len(frase))
+print(frase.lower())
+print(frase.upper())
+print(frase.replace('Python', 'Java'))
+print(frase.count('o'))
+print(frase.capitalize())
+print(frase.strip())
+print(frase.split())
+
+print("""Python is an easy to learn, powerful programming language. It has efficient high-level data structures and a simple but effective approach to object-oriented programming.""")
