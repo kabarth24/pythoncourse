95619a57abeafb59b15d5047d461ff32b0f4efe4
Luka <lukadbarth@hotmail.com>
2024-01-11 05:56:18 -0300

    I learned about conditional structure in Python, its syntax and logic

---
+++ World-1/conditional-structure.py
@@ -0,0 +1,10 @@
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+
+print(f'A sua média foi {media:.1f}')
+
+if media >= 6.0:
+  print('Sua média foi boa! PARABÉNS!')
+else:
+  print('Sua média foi ruim! ESTUDE MAIS!')
