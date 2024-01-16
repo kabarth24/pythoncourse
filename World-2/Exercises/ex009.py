ab5c583dd7d747c7a84d0fead939154c2aca3d2a
Luka <lukadbarth@hotmail.com>
2024-01-16 06:01:40 -0300

    This exercise consists of counting down from 10 to 0

---
+++ World-2/Exercises/ex009.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+print('✨' * 12)
+print('Contagem regressiva!!!')
+print('✨' * 12)
+
+for i in range(10, -1, -1):
+  print(i)
+  sleep(1)
+
+print('💥 BUM! BUM! POOOOOWW!! 💥' )
