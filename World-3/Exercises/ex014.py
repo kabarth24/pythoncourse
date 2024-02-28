9d81adf66c22ede2729cbc45f66c0cff33018a91
Luka <lukadbarth@hotmail.com>
2024-02-28 16:32:21 -0300

    This exercise consists of creating a function that will first count from 1 to 10, then from 10 to 1, jumping from 2 to 2 and finally the user can choose the values to run the program

---
+++ World-3/Exercises/ex014.py
@@ -0,0 +1,32 @@
+from time import sleep
+
+def contador(inicio, fim, passo):
+  print('-=' * 30)
+  print(f'Contagem de {inicio} até {fim} de {passo} em {passo}')
+  sleep(2.5)
+
+  if inicio < fim:
+    cont = inicio
+    while cont <= fim:
+      print(f'{cont} ', end='', flush=True)
+      sleep(0.5)
+      cont += passo
+    print('FIM!!!')
+  else:
+    cont = inicio
+    while cont >= fim:
+      print(f'{cont} ', end='', flush=True)
+      sleep(0.5)
+      cont -= passo
+    print('FIM!!!')
+
+
+contador(1, 10, 1)
+contador(10, 1, 2)
+
+print('Agora é a sua vez de personalizar a contagem!')
+ini = int(input('Início: '))
+f = int(input('Fim: '))
+pas = int(input('Passo: '))
+
+contador(ini, f, pas)
