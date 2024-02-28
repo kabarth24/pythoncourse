ba0453a504191d20dc487dd7dc52da22b81869dd
Luka <lukadbarth@hotmail.com>
2024-02-28 15:30:03 -0300

    This exercise consists of using the function to write a sentence and the embellishment lines follow the size of that sentence

---
+++ World-3/Exercises/ex013.py
@@ -0,0 +1,8 @@
+def escreva(txt):
+  print('~' * len(txt))
+  print(txt)
+  print('~' * len(txt))
+
+escreva('Olá, Mundo!')
+escreva('EU EU EU EU EU EU EU')
+escreva('Os tiozinhos estão acompanhando a frase!!')
